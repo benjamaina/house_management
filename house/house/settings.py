3d600a0d866c1233345b3dae43dee9ea04fa4871
benjamaina <161875568+benjamaina@users.noreply.github.com>
2025-05-15 12:00:46 +0300

    Added Docker to the project

--- house/house/settings.py
+++ house/house/settings.py
@@ -29,6 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False")== "True"
+REDIS_HOST = os.getenv("REDIS_URL")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -81,7 +83,7 @@ REST_FRAMEWORK = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis_cache:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
